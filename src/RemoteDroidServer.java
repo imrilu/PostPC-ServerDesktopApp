eedbe7155583708ac2b81517c749721a40475a42
rubi1993 <rubi.mizrahi@gmail.com>
2018-09-27 17:51:32 +0300

    Rejected commit: Default

--- src/RemoteDroidServer.java
+++ src/RemoteDroidServer.java
@@ -94,19 +94,19 @@ public class RemoteDroidServer {
                 }
 
                 //if user clicks on next
-                if(line.equalsIgnoreCase("next")){
+                if(line.equalsIgnoreCase("n")){
                     //Simulate press and release of key 'n'
                     robot.keyPress(KeyEvent.VK_N);
                     robot.keyRelease(KeyEvent.VK_N);
                 }
                 //if user clicks on previous
-                else if(line.equalsIgnoreCase("previous")){
+                else if(line.equalsIgnoreCase("p")){
                     //Simulate press and release of key 'p'
                     robot.keyPress(KeyEvent.VK_P);
                     robot.keyRelease(KeyEvent.VK_P);
                 }
                 //if user clicks on play/pause
-                else if(line.equalsIgnoreCase("play")){
+                else if(line.equalsIgnoreCase("space")){
                     //Simulate press and release of spacebar
                     robot.keyPress(KeyEvent.VK_SPACE);
                     robot.keyRelease(KeyEvent.VK_SPACE);
@@ -127,6 +127,127 @@ public class RemoteDroidServer {
                     //Simulate press and release of mouse button 1(makes sure correct button is pressed based on user's dexterity)
                     robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
                     robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+                }
+                else if(line.equalsIgnoreCase("q")){
+                    //Simulate press and release of key 'q'
+                    robot.keyPress(KeyEvent.VK_Q);
+                    robot.keyRelease(KeyEvent.VK_Q);
+                }
+                else if(line.equalsIgnoreCase("w")){
+                    //Simulate press and release of key 'w'
+                    robot.keyPress(KeyEvent.VK_W);
+                    robot.keyRelease(KeyEvent.VK_W);
+                }
+                else if(line.equalsIgnoreCase("e")){
+                    //Simulate press and release of key 'e'
+                    robot.keyPress(KeyEvent.VK_E);
+                    robot.keyRelease(KeyEvent.VK_E);
+                }
+                else if(line.equalsIgnoreCase("r")){
+                    //Simulate press and release of key 'r'
+                    robot.keyPress(KeyEvent.VK_R);
+                    robot.keyRelease(KeyEvent.VK_R);
+                }
+                else if(line.equalsIgnoreCase("t")){
+                    //Simulate press and release of key 't'
+                    robot.keyPress(KeyEvent.VK_T);
+                    robot.keyRelease(KeyEvent.VK_T);
+                }
+                else if(line.equalsIgnoreCase("y")){
+                    //Simulate press and release of key 'y'
+                    robot.keyPress(KeyEvent.VK_Y);
+                    robot.keyRelease(KeyEvent.VK_Y);
+                }
+                else if(line.equalsIgnoreCase("u")){
+                    //Simulate press and release of key 'u'
+                    robot.keyPress(KeyEvent.VK_U);
+                    robot.keyRelease(KeyEvent.VK_U);
+                }
+                else if(line.equalsIgnoreCase("i")){
+                    //Simulate press and release of key 'p'
+                    robot.keyPress(KeyEvent.VK_I);
+                    robot.keyRelease(KeyEvent.VK_I);
+                }
+                else if(line.equalsIgnoreCase("o")){
+                    //Simulate press and release of key 'o'
+                    robot.keyPress(KeyEvent.VK_O);
+                    robot.keyRelease(KeyEvent.VK_O);
+                }
+                else if(line.equalsIgnoreCase("a")){
+                    //Simulate press and release of key 'a'
+                    robot.keyPress(KeyEvent.VK_A);
+                    robot.keyRelease(KeyEvent.VK_A);
+                }
+                else if(line.equalsIgnoreCase("s")){
+                    //Simulate press and release of key 's'
+                    robot.keyPress(KeyEvent.VK_S);
+                    robot.keyRelease(KeyEvent.VK_S);
+                }
+                else if(line.equalsIgnoreCase("d")){
+                    //Simulate press and release of key 'd'
+                    robot.keyPress(KeyEvent.VK_D);
+                    robot.keyRelease(KeyEvent.VK_D);
+                }
+                else if(line.equalsIgnoreCase("f")){
+                    //Simulate press and release of key 'f'
+                    robot.keyPress(KeyEvent.VK_F);
+                    robot.keyRelease(KeyEvent.VK_F);
+                }
+                else if(line.equalsIgnoreCase("g")){
+                    //Simulate press and release of key 'g'
+                    robot.keyPress(KeyEvent.VK_G);
+                    robot.keyRelease(KeyEvent.VK_G);
+                }
+                else if(line.equalsIgnoreCase("h")){
+                    //Simulate press and release of key 'h'
+                    robot.keyPress(KeyEvent.VK_H);
+                    robot.keyRelease(KeyEvent.VK_H);
+                }
+                else if(line.equalsIgnoreCase("j")){
+                    //Simulate press and release of key 'j'
+                    robot.keyPress(KeyEvent.VK_J);
+                    robot.keyRelease(KeyEvent.VK_J);
+                }
+                else if(line.equalsIgnoreCase("k")){
+                    //Simulate press and release of key 'k'
+                    robot.keyPress(KeyEvent.VK_K);
+                    robot.keyRelease(KeyEvent.VK_K);
+                }
+                else if(line.equalsIgnoreCase("l")){
+                    //Simulate press and release of key 'l'
+                    robot.keyPress(KeyEvent.VK_L);
+                    robot.keyRelease(KeyEvent.VK_L);
+                }
+                else if(line.equalsIgnoreCase("z")){
+                    //Simulate press and release of key 'z'
+                    robot.keyPress(KeyEvent.VK_Z);
+                    robot.keyRelease(KeyEvent.VK_Z);
+                }
+                else if(line.equalsIgnoreCase("x")){
+                    //Simulate press and release of key 'x'
+                    robot.keyPress(KeyEvent.VK_X);
+                    robot.keyRelease(KeyEvent.VK_X);
+                }
+                else if(line.equalsIgnoreCase("c")){
+                    //Simulate press and release of key 'c'
+                    robot.keyPress(KeyEvent.VK_C);
+                    robot.keyRelease(KeyEvent.VK_C);
+                }   else if(line.equalsIgnoreCase("v")){
+                    //Simulate press and release of key 'v'
+                    robot.keyPress(KeyEvent.VK_V);
+                    robot.keyRelease(KeyEvent.VK_V);
+                }   else if(line.equalsIgnoreCase("b")){
+                    //Simulate press and release of key 'b'
+                    robot.keyPress(KeyEvent.VK_B);
+                    robot.keyRelease(KeyEvent.VK_B);
+                }   else if(line.equalsIgnoreCase("n")){
+                    //Simulate press and release of key 'n'
+                    robot.keyPress(KeyEvent.VK_N);
+                    robot.keyRelease(KeyEvent.VK_N);
+                }   else if(line.equalsIgnoreCase("m")){
+                    //Simulate press and release of key 'm'
+                    robot.keyPress(KeyEvent.VK_M);
+                    robot.keyRelease(KeyEvent.VK_M);
                 }
                 //Exit if user ends the connection
                 else if(line.equalsIgnoreCase("exit")){
